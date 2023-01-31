c9b947d63d8fb8ab85534ae994ec21d288d21143
ViniciusIth <vini.ithalo123@gmail.com>
2023-01-31 15:10:44 -0300

    First Commit

---
+++ src/client/java/viniciusith/shadowgrave/ShadowGraveModClient.java
@@ -0,0 +1,10 @@
+package viniciusith.shadowgrave;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ShadowGraveModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+    }
+}
